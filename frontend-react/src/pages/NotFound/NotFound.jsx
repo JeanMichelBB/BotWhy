57ba5c00803d69db4cb305bc74faefb75b3a2801
JeanMichelBB <jeanmichelbberube@outlook.com>
2024-10-31 11:48:25 -0400

    test github action

--- frontend-react/src/pages/NotFound/NotFound.jsx
+++ frontend-react/src/pages/NotFound/NotFound.jsx
@@ -8,6 +8,7 @@ const NotFound = () => {
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
       <Link to="/" className="not-found__link">Go to Home</Link>
+      <p style={{ color: 'red' }}>this is a text</p>
     </div>
   );
 };
