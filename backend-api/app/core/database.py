9d755e1409824e1261c87636e228ea6c16a352a4
JeanMichelBB <jeanmichelbberube@outlook.com>
2025-02-14 10:35:25 -0400

    update connection db

--- backend-api/app/core/database.py
+++ backend-api/app/core/database.py
@@ -13,10 +13,8 @@ load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
 
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@mysql/chatbox_db"
 
 # Create the SQLAlchemy engine and session
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
