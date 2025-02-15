02b87c7ed4a2f5a11ceb36051b176f0b97b90f7c
JeanMichelBB <jeanmichelbberube@outlook.com>
2025-02-15 11:06:58 -0400

    update secret naming

--- frontend-react/src/api.jsx
+++ frontend-react/src/api.jsx
@@ -1,3 +1,3 @@
-export const apiKey = import.meta.env.VITE_API_KEY;
-export const apiUrl = import.meta.env.VITE_API_URL;
-export const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+export const apiKey = import.meta.env.VITE_APP_API_KEY;
+export const apiUrl = import.meta.env.VITE_APP_API_URL;
+export const googleClientId = import.meta.env.VITE_APP_GOOGLE_CLIENT_ID;
