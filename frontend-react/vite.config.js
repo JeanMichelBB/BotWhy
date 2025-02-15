7899404b72c5c42f86ef7e78c9eeebe1534a1c4d
JeanMichelBB <jeanmichelbberube@outlook.com>
2025-02-15 12:15:03 -0400

    update github action

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,22 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      // Proxy configuration for API requests
-      '/api': {
-        target: 'http://localhost:8000', // Replace with your backend server URL
-        changeOrigin: true,
-        configure: (proxy) => {
-          proxy.on('proxyRes', (proxyRes) => {
-            proxyRes.headers['Referrer-Policy'] = 'no-referrer-when-downgrade';
-          });
-        },
-      },
-    },
+    host: '0.0.0.0',
   },
-});
+  build: {
+    chunkSizeWarningLimit: 10000,
+    outDir: 'dist', // This specifies the output directory
+    sourcemap: false,
+  }
+})
